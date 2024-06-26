dfd2a3b4a7c14b042d258c8d17658c83f33efa30
Gabriel <135087253+Gabriel-Tan-8889@users.noreply.github.com>
2024-06-26 13:37:02 +0000

    Made level 3

--- level3.py
+++ level3.py
@@ -1,1 +1,26 @@
+import time
 print("Welcome to Level 3. Good Luck!")
+
+print("You are a dective and you are trying to find a killer")
+print("You are in a room with a suspect, you have to find the killer")
+print("There are 5 susupect Mandy, Josh, John, Lucas and Evan")
+
+print("You ask each person where they were during the murder")
+print("Mandy: I was at the party")
+print("John: I was marking my students paper")
+print("Lucas: I was at the gym")
+print("Josh: I was at the bar eating blue cheese and white wine")
+print( "Evan: I was at the football feild.")
+
+personanswer = input("Who do you think is the killer?")
+
+if personanswer == "Josh":
+  print("You are correct. Congruagulations.")
+  continu = input("Do you want to continue?")
+  if continu == "yes":
+    import level4
+else:
+  print("You are incorrect. The killer kills you soon after.")
+  print("Heading to lobby")
+  time.sleep(5)
+  import main
