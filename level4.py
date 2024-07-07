8b11fa3b7ecd4b8f63ae4d6854926625feba5413
Gabriel <135087253+Gabriel-Tan-8889@users.noreply.github.com>
2024-07-07 12:48:01 +0000

    Added more lore to the plot

--- level4.py
+++ level4.py
@@ -1,1 +1,12 @@
-print("Welcome to Level 4. Good Luck!")
+print("Congragulations! You survived the frist 3 levels")
+print("You find yourself in a room with a portrait of a man and you realise it was Evan.")
+print("""A person comes in. He says: "Evan was a evil warlord, wanting the blade of deception to take our the world 100 years ago. However, he 3 warlords who created it fought him and he was defeated. Lengend says that his soul is still alive, hiding, bidding his time to take over the world. After the war, the cosmic warlords created a sword, the sword of light, to combat should someone use the blade of deception ever again. They hid both swords to increase the chance of swords not being used """)
+print("""Only a person of pure heart can weild the sword of light. Rumour hs it if Evan has been searching for the sword of deception. If Evan returns with the sword, the world will be destroyed.""")
+print("The the sirens in the place start to sound, Evan found the sword of deception.")
+print("The warlords placed clues to find the sword of light. Goodbye")
+contin = input("Do you want to continue?")
+if contin == "yes":
+  import level5
+if contin == "no":
+  import main
+  
